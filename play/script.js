53d5a6131bfc85cee6c124a71294d1f503f904e6
orecraft-game <78282416+orecraft-game@users.noreply.github.com>
2021-01-30 16:33:04 -0700

    Add files via upload

---
+++ play/script.js
@@ -0,0 +1,13 @@
+function login(form) {
+	var usr = form.username.value;
+	var psw = form.password.value;
+	window.location = "https://orecraft.dobbyk.repl.co/play/game";
+
+
+}
+function signup(form) {
+	var usr = form.username.value;
+	var psw = form.password.value;
+	var email = form.email.value;
+
+}
